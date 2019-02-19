f2ba0acf91688af91c5ee46dbbf0e33f26a1f57a
Dmitry Shumilin <vexmit@gmail.com>
2019-02-19 14:43:12 +0300

    Partials folder added

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-// EXRPESS
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // PORT
 const port = 3000;
 
-// BODY-PARSER
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-
-// Mongoose
-const mongoose = require('mongoose');
+// MONGOOSE CONNECTION
 mongoose.connect("mongodb://localhost:27017/app", {useNewUrlParser: true});
 
-// STATIC FILES & EJS
+// STATIC FILES, BODY-PARSER & EJS
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-// SCHEMA SETUP & MODEL
+// SCHEMA SETUP & MONGOOSE MODEL
 const appSchema = new mongoose.Schema({
     name: String,
 });
 
 const Myapp = mongoose.model('Myapp', appSchema);
 
-// GET INDEX.EJS
+// ROUTES
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -32,5 +29,5 @@ app.get('/', (req, res) => {
 
 // APP LISTEN
 app.listen(port, () => {
-    console.log("The server has started on port: " + port);
+    console.log("The server is working on port: " + port);
 });
